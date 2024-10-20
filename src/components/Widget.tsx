a320082a96290216a42b127ad18d6c4b7af0b227
softK1T <nazar12522@gmail.com>
2024-10-20 04:09:41 +0200

    Fixed with animation

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -2,38 +2,34 @@ import React, { useState } from "react";
 
 interface WidgetProps {
   children: React.ReactNode;
-  onRemove?: () => void; // Add a prop for handling widget removal
+  onRemove?: () => void;
 }
 
 const Widget: React.FC<WidgetProps> = ({ children, onRemove }) => {
   const [isVibrating, setIsVibrating] = useState(false);
 
-  // Trigger vibration when the left mouse button is pressed
   const handleMouseDown = (event: React.MouseEvent) => {
     if (event.button === 0) {
-      // 0 means the left mouse button
       setIsVibrating(true);
     }
   };
 
-  // Stop vibration when the mouse button is released
   const handleMouseUp = () => {
     setIsVibrating(false);
   };
 
-  // Stop vibration when the mouse leaves the widget
   const handleMouseLeave = () => {
-    // setIsVibrating(false);
+    // setIsVibrating(false); // Ensure vibration stops when the mouse leaves
   };
 
   return (
     <div
-      className={`flex-grow shadow-xl text-primary select-none flex-shrink basis-1 p-6 w-auto h-full rounded-lg border bg-card font-sans rounded-[40px]${
+      className={`relative flex-grow shadow-xl text-primary select-none flex-shrink basis-1 p-6 w-auto h-full rounded-lg border bg-card font-sans rounded-[40px] ${
         isVibrating ? "vibrate_small" : ""
-      }`} // Removed scale and hover effects
-      onMouseDown={handleMouseDown} // Start vibrating when LMB is pressed
-      onMouseUp={handleMouseUp} // Stop vibrating on mouse up
-      onMouseLeave={handleMouseLeave} // Stop vibrating on mouse leave
+      }`} // Apply the vibration class when `isVibrating` is true
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseLeave}
     >
       {children}
       {onRemove && (
