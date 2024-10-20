d78c12393cd607f553b0629e8ce1c49e29a91b34
softK1T <nazar12522@gmail.com>
2024-10-20 03:57:43 +0200

    Fixed with working removing

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -91,6 +91,19 @@ const Dashboard: React.FC<DashboardProps> = ({
     }),
   });
 
+  const disableDraggableOnHover = (id: string, disable: boolean) => {
+    setLayout((prevLayout) =>
+      prevLayout.map((widgetLayout) =>
+        widgetLayout.i === id
+          ? { ...widgetLayout, isDraggable: !disable }
+          : widgetLayout
+      )
+    );
+  };
+  const handleWidgetRemove = (id: string) => {
+    onWidgetRemove(id); // Remove the widget
+  };
+
   const handleLayoutChange = (newLayout: Layout[]) => {
     setLayout(newLayout);
     onLayoutChange(newLayout); // Update layout in the parent
